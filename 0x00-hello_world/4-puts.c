d409df53cafb25851b3cb0e14b7f5378cb657fee
brianMutea <brianmuteak@gmail.com>
2022-07-04 03:56:43 -0700

    print string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: always 0(Success)
+ */
+
+int main(void)
+
+{
+	char string[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(string);
+	return (0);
+}
