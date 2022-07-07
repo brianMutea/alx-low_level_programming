e5f160ab64eaa62e9ea91001e57cca7bf5ee0ddb
brianMutea <brianmuteak@gmail.com>
2022-07-07 06:37:44 -0700

    fix largest of the nums

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -1,6 +1,5 @@
-#include <stdio.h>
 #include "main.h"
-
+#include <stdio.h>
 /**
  * main - prints the largest of 3 integers
  * Return: 0(success)
@@ -16,6 +15,8 @@ int main(void)
 	c = 0;
 
 	largest = largest_number(a, b, c);
+
 	printf("%d is the largest number\n", largest);
+
 	return (0);
 }
