cf144cbf9ebc389d27074d3be33a6664cb3b96bb
brianMutea <brianmuteak@gmail.com>
2022-07-08 19:55:28 -0700

    print_lines _

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - prints _
+ * @n: param
+ * Return: nothing
+ */
+
+void print_line(int n)
+{
+	while (n-- > 0)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
